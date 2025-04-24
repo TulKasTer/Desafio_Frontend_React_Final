e477f4a78a47554021e15629fa6c003664fd5018
Nick-2455 <nicolaspacheco2455@gmail.com>
2025-04-24 16:37:05 -0700

    cambios extras

--- Desafio_Frontend_React_Final/src/pages/inicio.tsx
+++ Desafio_Frontend_React_Final/src/pages/inicio.tsx
@@ -1,9 +1,8 @@
-// src/pages/inicio.tsx
 import React from "react";
-import SideComp from "../components/SidebarComp"; // Importación correcta
-import CategoryPieChart from "../components/CategoryPieChart"; // Importación correcta
-import SuppliersPieChart from "../components/SuppliersPieChart"; // Importación correcta
-import ProductQuantityBarChart from "../components/ProductQuantityBarChart"; // Importación correcta
+import SideComp from "../components/SidebarComp"; 
+import CategoryPieChart from "../components/CategoryPieChart"; 
+import SuppliersPieChart from "../components/SuppliersPieChart"; 
+import ProductQuantityBarChart from "../components/ProductQuantityBarChart"; 
 
 export default function Inicio() {
   return (
@@ -13,21 +12,16 @@ export default function Inicio() {
         <main className="main-content flex flex-col sm:flex-row flex-grow p-3 mx-40 gap-6">
           <div className="content text-2xl font-bold w-full">
 
-            {/* Contenedor flex para alinear horizontalmente las gráficas */}
             <div className="flex flex-col sm:flex-row gap-6 justify-between w-full">
-              
-              {/* Contenedor para la gráfica de productos por categoría */}
               <div className="flex-1 sm:max-w-[3000px] p-4 bg-white border border-gray-300 rounded-lg shadow-lg">
                 <CategoryPieChart />
               </div>
 
-              {/* Contenedor para la gráfica de cantidad de cada producto */}
               <div className="flex-1 sm:max-w-[3000px] p-4 bg-white border border-gray-300 rounded-lg shadow-lg">
                 <SuppliersPieChart />
               </div>
             </div>
 
-            {/* Contenedor para la gráfica de productos por proveedor */}
             <div className="flex flex-col sm:flex-row w-full p-4 bg-white border border-gray-300 rounded-lg shadow-lg mt-6">
               <div className="w-full">
                 <ProductQuantityBarChart />
